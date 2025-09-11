6836450b6ca39c2873845eb831520b7d64d442df
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-09-11 11:09:10 +0100

    Anna Ban@na

--- 0x01-javascript_algorithms_and_data_structures/00-palindrome_checker/palindrome_checker.js
+++ 0x01-javascript_algorithms_and_data_structures/00-palindrome_checker/palindrome_checker.js
@@ -1,1 +1,56 @@
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
+const validateUserInput = () => {
+  if (!textInput.value.trim()) {
+    result.classList.add("hide");
+    alert("Please input a value.");
+  } else {
+    result.innerHTML = `<p>Ooops! Wanna try again?</p>`;
+    result.classList.remove("hide");
+  }
+};
+
+const isPalindrome = () => {
+  const userInput = textInput.value;
+  const requiredPattern = /[a-z0-9]/gi;
+
+  if (requiredPattern.test(userInput)) {
+    const cleanString = userInput.toLowerCase().match(requiredPattern).join("");
+    const reversedString = userInput
+      .toLowerCase()
+      .match(requiredPattern)
+      .reverse()
+      .join("");
+
+    if (cleanString === reversedString) {
+      result.innerHTML = `<p>${userInput} is a palindrome.</p>`;
+    } else {
+      result.innerHTML = `<p>${userInput} is not a palindrome.</p>`;
+    }
+
+    result.classList.remove("hide");
+    textInput.value = "";
+  } else {
+    validateUserInput();
+  }
+};
+
+/*
+const clearResult = (e) => {
+  if (e.target.value) {
+    result.classList.add("hide");
+  }
+};
+*/
+
+checkButton.addEventListener("click", isPalindrome);
+
+textInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    isPalindrome();
+  }
+});
+
+// textInput.addEventListener("input", clearResult);
